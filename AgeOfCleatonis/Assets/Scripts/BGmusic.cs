bbc9cdd056e51dcea6a0d40e4cd1253ab0f39db6
RokoS14 <80956925+RokoS14@users.noreply.github.com>
2022-04-22 16:58:00 +0200

    roko

---
+++ AgeOfCleatonis/Assets/Scripts/BGmusic.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGmusic : MonoBehaviour
+{
+    public static BGmusic instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(this);
+    }
+}
